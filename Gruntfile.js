9debbeb0e80c0b55a6c2470d0dbd41829dfcb30d
Thomas Ruble <sketchbang@gmail.com>
2016-02-01 10:29:07 -0600

    at page 47

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,6 +28,7 @@ module.exports = function (grunt) {
                     before_tests: 'grunt jshint',
                     serve_files: [
                         'node_modules/lodash/index.js',
+                        'node_modules/lodash/lodash.js',
                         'node_modules/jquery/dist/jquery.js',
                         'node_modules/sinon/pkg/sinon.js',
                         'src/**/*.js',
---
+++ src/scope.js
@@ -0,0 +1,31 @@
+/* jshint globalstrict: true */
+'use strict';
+
+function Scope() {
+    this.$$watchers = [];
+}
+function initWatchVal() {}
+
+Scope.prototype.$watch = function(watchFn, listenerFn) {
+    var watcher = {
+        watchFn: watchFn,
+        listenerFn: listenerFn || function() {},
+        last: initWatchVal
+    };
+    this.$$watchers.push(watcher);
+};
+
+Scope.prototype.$digest = function() {
+    var self = this;
+    var newValue, oldValue;
+    _.forEach(this.$$watchers, function(watcher) {
+        newValue = watcher.watchFn(self);
+        oldValue = watcher.last;
+        if (newValue !== oldValue) {
+            watcher.last = newValue;
+            watcher.listenerFn(newValue, 
+              (oldValue == initWatchVal ? newValue : oldValue), self);
+        }
+    });
+   
+};
